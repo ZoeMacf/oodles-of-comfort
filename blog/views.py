48ca35807ea5d4501b612e109ef9e1678936c8fa
Zoe Macf <zoemacfarlane93@gmail.com>
2024-02-19 21:13:17 +0000

    Display a recipe to recipe_list.html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
 # Create your views here.
+
+class RecipeList(generic.ListView):
+    model = Recipe
