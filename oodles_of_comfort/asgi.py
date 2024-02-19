a5f238d13a617bfd143a46dbba5b14a814645065
Zoe Macf <zoemacfarlane93@gmail.com>
2024-02-19 13:46:02 +0000

    Initial commit

---
+++ oodles_of_comfort/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for oodles_of_comfort project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'oodles_of_comfort.settings')
+
+application = get_asgi_application()
