94bef99e581348d45dbafa5da7024c7fbbb53c66
Zoe Macf <zoemacfarlane93@gmail.com>
2024-02-19 14:46:08 +0000

    create recipe model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+    User, on_delete=models.CASCADE, related_name="recipe_posts"
+)
+    prep_time = models.CharField(max_length=30, unique=False)
+    cook_time = models.CharField(max_length=30, unique=False)
+    ingredients = models.CharField(max_length=200, unique=False)
+    recipe_steps = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
