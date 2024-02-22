0d3d1beeeb8b4e48ef4c43051af6fe56d2df70dd
Zoe Macf <zoemacfarlane93@gmail.com>
2024-02-22 16:30:26 +0000

    install cloudinary and connect to database

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -17,3 +18,4 @@ class Recipe(models.Model):
     recipe_steps = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
+    featured_image = CloudinaryField('image', default='placeholder')
