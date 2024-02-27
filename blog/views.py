b6f6c446575dbb889b10188a0836c104401087ad
Zoe Macf <zoemacfarlane93@gmail.com>
2024-02-27 23:08:13 +0000

    add recipe detail page

--- blog/views.py
+++ blog/views.py
@@ -5,5 +5,12 @@ from .models import Recipe
 # Create your views here.
 
 class RecipeList(generic.ListView):
+    """View all of the recipes"""
     model = Recipe
     template_name = "blog/index.html"
+
+class RecipeDetail(generic.DetailView):
+    """View a single recipe"""
+    model = Recipe
+    template_name = "blog/recipe_detail.html"
+    
