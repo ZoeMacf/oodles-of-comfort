363db0abaff89cf17f346c1942ba7bf9718a667f
Zoe Macf <zoemacfarlane93@gmail.com>
2024-03-05 23:23:09 +0000

    create instance of UserProfile when user signs up

---
+++ users/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.contrib.auth.models import User 
+from django.dispatch import receiver 
+from .models import UserProfile
+
+# creation and setting up of signals file - https://www.devhandbook.com/django/user-profile/
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
