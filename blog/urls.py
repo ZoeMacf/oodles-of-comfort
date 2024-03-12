636deda45c55bd975ad6a9ef2061aaba32fba802
Zoe Macf <zoemacfarlane93@gmail.com>
2024-03-12 20:39:07 +0000

    add function to delete comment when logged in

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,4 @@ urlpatterns = [
     path("recipes", views.RecipeList.as_view(), name="recipe_list"),
     path("recipe_search", views.recipe_search, name="recipe_search"),
     path('<slug:slug>/edit_comment/<int:comment_id>',views.edit_comment, name='edit_comment'),
-]
+    path('<slug:slug>/delete_comment/<int:comment_id>',views.comment_delete, name='comment_delete'),]
--- oodles_of_comfort/settings.py
+++ oodles_of_comfort/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-zoemacf-oodlesofcomfort-flxt9dbdnjx.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-zoemacf-oodlesofcomfort-flxt9dbdnjx.ws-eu109.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
